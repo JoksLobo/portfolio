b0be2f5424f122c0f91f7437b4617e8a9ce31594
JoksLobo <jocson_herry@hotmail.com>
2025-02-14 17:50:38 -0300

    estilizando a sidebar

---
+++ src/Components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { ProfilePic } from './styles.ts'
+
+const Avatar = () => <ProfilePic src="https://github.com/JoksLobo.png" alt="" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Titulo from '../../Components/Titulo/index.tsx'
+import Avatar from '../../Components/Avatar/index.tsx'
+import Paragrafo from '../../Components/Paragrafo/index.tsx'
+
+import { Descricao, ThemeButton, SidebarContainer } from './styles.ts'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/JoksLobo.png" alt="" />
-    <Titulo fontSize={20}>Joks Lobo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Joks Lobo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        JoksLobo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <ThemeButton>Trocar tema</ThemeButton>
+    </SidebarContainer>
   </aside>
 )
 
